c20d173b72324f59a2ac4d5b4c2ab500ca3dfb67
Ali Arshadi <realgabral@protonmail.com>
2025-05-05 18:23:15 +0430

    BaseInput created

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import BaseButton from '@/components/base/BaseButton.vue';
+import BaseInput from '@/components/base/BaseInput.vue';
 </script>
 
 <template>
   <div>
     <BaseButton variant="ghost" size="lg"> this is a button </BaseButton>
+    <BaseInput type="email" placeholder="you@example.com" />
   </div>
 </template>
